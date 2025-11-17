f35d511f3c975683489162a5c77bd19239267f77
kj77kj7 <kj77kj7@gmail.com>
2025-11-17 15:55:02 +0900

    fff

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/web5/',   // 레포 이름 그대로 적기
   plugins: [react()],
-})
+  base: "/web5/", // ⚠️ 레포 이름 정확히 넣기!!
+});
